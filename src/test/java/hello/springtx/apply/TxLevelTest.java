e8331a643d1445791d081b5b2786d7bf5470a745
InimitablePioneer <seungwonpioneer@gmail.com>
2025-04-17 11:09:34 +0900

    APR17

---
+++ src/test/java/hello/springtx/apply/TxLevelTest.java
@@ -0,0 +1,14 @@
+package hello.springtx.apply;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TxLevelTest {
+
+
+    static class LevelService {
+
+
+    }
+}
